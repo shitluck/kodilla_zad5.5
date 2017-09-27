4f08e05ea0325949ddec34c350f688b5e86b520f
Mac <fuji.tw6@gmail.com>
2017-09-27 16:56:17 +0200

    add PUG

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+app.set('view engine', 'pug');
+app.set('views','./views');
+
 app.use('/store',function(req, res, next) {
 	console.log('Jestem pośrednikiem przy żądaniu do /store');
 	next();
@@ -13,6 +16,24 @@ app.get('/', function(req, res) {
 
 app.get('/store', function(req, res) {
 	res.send('To jest sklep');
+});
+
+app.get('/first-view', function(req, res) {
+    res.render('first-view');
+});
+
+app.get('/dynamic-view', function(req, res) {
+    res.render('dynamic', {
+        name: "Moja dynamiczna strona",
+        url: "http://www.google.com"
+    });
+});
+
+app.get('/dynamic', function(req, res) {
+    res.render('dynamic', {
+        user:
+        { name: "Johnny", age: "20" }
+    });
 });
 
 app.listen(3000);
